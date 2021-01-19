eab22a0a9d2f9f6a40f660bacb21fe7d293ee34c
darrickpang <dkypang@yahoo.com>
2021-01-18 17:03:18 -0800

    chemical CRUD

--- src/components/Chemical.js
+++ src/components/Chemical.js
@@ -9,6 +9,62 @@ class Chemical extends React.Component {
         time: null 
     }
     
+    handleSubmit = (e, addDate, updateDate, deleteDate) => {
+        e.preventDefault()
+        let {class_name, date} = this.state
+        if(class_name !== null && date !== null){
+            let date_info = {
+                class_name: class_name,
+                date: date,
+                student_id: parseInt(this.props.student.id)
+            }
+            // persist to database
+            if(this.state.dateAdd){
+                addDate(date_info)
+            } 
+            else if(!this.state.dateAdd && e.target.name === "update"){
+                updateDate(this.state.id, date_info)
+            }
+            else {
+                deleteDate(this.state.id, date_info)
+            }
+            // reset state
+            this.setState({
+                id: null,
+                class_name: null,
+                date: null,
+                dateAdd: true,
+                deleteDate: false
+            })
+            e.target.parentElement.reset()
+        }
+        else{
+            alert("You must include a class name and date to create a new schedule.")
+        }
+    }
+
+    autoFillForm = (selectedValue, dates) => {
+        if(selectedValue === "n/a"){
+            this.setState({
+                id: null,
+                class_name: null,
+                date: null,
+                dateAdd: true,
+                deleteDate: false
+            })
+        }
+        else{
+            let find_date = dates.find(date_info => date_info.id == selectedValue)
+            this.setState({
+                id: find_date.id,
+                class_name: find_date.class_name,
+                date: find_date.date,
+                dateAdd: false,
+                deleteDate: true
+            })
+        }
+    }
+
     render(){
         return(
             <div>
